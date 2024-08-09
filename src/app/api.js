f0bf0f6609f36ca2b654536316ebbddd96a7e976
vivek kumar <vivekr4400@gmail.com>
2024-08-09 13:18:58 +0530

    updated

--- src/app/api.js
+++ src/app/api.js
@@ -1,6 +1,3 @@
-import axios from "axios"
-
-
 
 export const getUsers=async()=>{
     try {
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -4,7 +4,12 @@ import { getUsers } from "../api"
 
  const Users=async()=>{
     const users=await getUsers()
-    console.log(users);
+     console.log('found users', users);
+     
+     if (!users) {
+         return <h1>No user Found</h1>
+     }
+
      return (
        <>
      <ul>
